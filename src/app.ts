48028ac5568457f091c71489284dbf19b0047ddd
Guilfornari <guilfornari@gmail.com>
2023-09-30 15:50:11 -0300

    app listen & health endppoint

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
-const app = express();
-app.use(cors())
-app.use(express.json());
+import { Request, Response } from "express";
 
 dotenv.config();
 
-app.get("/health", (req, res) => {
-    res.send("Server is alive and well!");
-});
+const app = express();
+app
+    .use(cors())
+    .use(express.json())
+    .get("/health", (req: Request, res: Response) => res.send("Server is alive and well!"));
 
 const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => console.log(`Running server on port ${PORT}`));
